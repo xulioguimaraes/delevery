ccc68d45be5cdf4560b61e76563955f9ef6ec50c
Julio <despairnatural@gmail.com>
2022-07-16 15:59:18 -0400

    Footer dev

--- src/components/FooterPage/FooterPage.tsx
+++ src/components/FooterPage/FooterPage.tsx
@@ -1,11 +1,12 @@
-import { Box, Container, Link, Typography } from '@mui/material'
+import { Box, Container, Icon, Link, Stack, Typography } from '@mui/material'
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="#fff" align="center">
       {'Copyright © '}
-      <Link color="#fff" href="https://mui.com/">
-       Your
+      <Link color="#fff" href="https://www.instagram.com/xulioguimaraes/">
+        Julio C. Guimarães
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -16,17 +17,29 @@ export const FooterPage = () => {
   return (
     <Box position="relative" bottom="0" width="100%" component="footer" >
       <Container maxWidth="lg" sx={{ bgcolor: "#222", py: 2 }}>
-        {/* <Typography variant="h6" align="center" gutterBottom>
-          Julio C. Guimarães
-        </Typography> */}
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="#FFF"
-          component="p"
-        >
-          Julio C. Guimarães
-        </Typography>
+
+        <Stack
+          alignItems="center"
+          py={1}
+          justifyContent="center"
+          spacing={2}
+          direction='row'>
+          <Icon>
+            <Link  color="#fff"  href='https://www.instagram.com/xulioguimaraes/'>
+              <FaInstagram />
+            </Link>
+          </Icon>
+          <Icon>
+            <Link  color="#fff"  href='https://www.linkedin.com/in/xulioguimaraes/'>
+              <FaLinkedin />
+            </Link>
+          </Icon>
+          <Icon>
+            <Link  color="#fff"  href='https://github.com/xulioguimaraes'>
+              <FaGithub />
+            </Link>
+          </Icon>
+        </Stack>
         <Copyright />
       </Container>
     </Box>
